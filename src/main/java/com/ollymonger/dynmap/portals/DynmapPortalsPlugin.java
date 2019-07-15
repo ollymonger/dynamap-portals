11b90943a126eca690eeef2a038a70b65e6d8270
Oliver Monger <47275398+ollymonger@users.noreply.github.com>
2019-07-15 17:10:23 +0100

    Create marker icon when creating Nether Portal (#1)
    * onPortalCreate now creates Dynmap Marker
    
    * Removed unused imports
    
    * Fixed formatting issue with portalID
    
    * Changed coordinates back to floats
    
    * Created Nether Portals in Dynmap successfully
    
    * Replaced World world

--- src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
+++ src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
@@ -2,16 +2,18 @@ package com.ollymonger.dynmap.portals;
 
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.DynmapCommonAPI;
 import org.dynmap.markers.MarkerAPI;
 import org.dynmap.markers.MarkerSet;
+
+import java.util.List;
+
 
 public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
     static final String DYNMAP_PLUGIN_NAME = "dynmap";
@@ -34,11 +36,31 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onPortalCreate(PortalCreateEvent event) {
         getLogger().info("Portal created");
+        List<BlockState> blocks = event.getBlocks();
+
+        for (int i = 0; i < blocks.size(); i++) {
+            Block block = blocks.get(i).getBlock();
+
+            if (block.getType().name() != "FIRE") { //if portal block is not fire
+                continue;//continue on until fire is hit
+            }
+
+            String worldName = block.getWorld().getName();
+            float x = block.getX();
+            float y = block.getY();
+            float z = block.getZ();
+
+            String portalID = String.format("portal_%s_%d_%d_%d", worldName, Math.round(x), Math.round(y), Math.round(z));
+
+            this.portalSet.createMarker(portalID, "Nether Portal", worldName, x, y, z, markerApi.getMarkerIcon("portal"), true);
+            getLogger().info("Created Nether Portal: " + portalID);
+        }
     }
 
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event) {
         if (event.getBlock().getType().equals(Material.NETHER_PORTAL) == false) {
+
             return;
         }
 
