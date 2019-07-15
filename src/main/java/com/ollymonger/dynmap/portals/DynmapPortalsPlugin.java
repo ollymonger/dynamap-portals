4d87b14c43f6b58415234af13562d411a62ed52b
James Monger <jameskmonger@hotmail.co.uk>
2019-07-15 07:54:55 +0100

    Initialise set

--- src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
+++ src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
@@ -4,20 +4,25 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.DynmapCommonAPI;
 import org.dynmap.markers.MarkerAPI;
+import org.dynmap.markers.MarkerSet;
 
 public class DynmapPortalsPlugin extends JavaPlugin {
     static final String DYNMAP_PLUGIN_NAME = "dynmap";
 
+    static final String SET_ID_PORTALS = "nether_portals";
+    static final String SET_NAME_PORTALS = "Nether Portals";
+
     private MarkerAPI markerApi;
+    private MarkerSet portalSet;
 
     @Override
     public void onEnable() {
         getLogger().info("DynmapPortalsPlugin is now enabled");
 
-        this.getMarkerApi();
+        this.initialiseMarkerApi();
     }
 
-    private void getMarkerApi() {
+    private void initialiseMarkerApi() {
         if (Bukkit.getPluginManager().isPluginEnabled(DYNMAP_PLUGIN_NAME) == false) {
             throw new IllegalStateException("No Dynmap plugin found");
         }
@@ -30,5 +35,15 @@ public class DynmapPortalsPlugin extends JavaPlugin {
 
         this.markerApi = plugin.getMarkerAPI();
         getLogger().info("Marker API retrieved");
+
+        this.portalSet = this.markerApi.getMarkerSet(SET_ID_PORTALS);
+
+        if (this.portalSet == null) {
+            getLogger().info("Set not found, creating new set");
+            this.portalSet = this.markerApi.createMarkerSet(SET_ID_PORTALS, SET_NAME_PORTALS, null, true);
+        }
+
+        this.portalSet.setHideByDefault(false);
+        getLogger().info("Set initialised");
     }
 }
