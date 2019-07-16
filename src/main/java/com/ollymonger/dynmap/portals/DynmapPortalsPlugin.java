1e43f541b830f247d20866c782713928460fb197
Olly Monger <olly@yllo.co.uk>
2019-07-16 10:18:34 +0100

    Exclusion zones around portals

--- src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
+++ src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
@@ -21,8 +21,13 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
     static final String SET_ID_PORTALS = "nether_portals";
     static final String SET_NAME_PORTALS = "Nether Portals";
 
+    static final String SET_ID_PORTAL_EXCLUSIONS = "nether_portal_exclusions";
+    static final String SET_NAME_PORTAL_EXCLUSIONS = "Nether Portal Exclusions";
+
     private MarkerAPI markerApi;
     private MarkerSet portalSet;
+    private MarkerSet portalExclusionSet;
+    private List<BlockState> blockList;
 
     @Override
     public void onEnable() {
@@ -37,7 +42,7 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
     public void onPortalCreate(PortalCreateEvent event) {
         getLogger().info("Portal created");
         List<BlockState> blocks = event.getBlocks();
-
+        this.blockList = blocks;
         for (int i = 0; i < blocks.size(); i++) {
             Block block = blocks.get(i).getBlock();
 
@@ -51,21 +56,23 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
             float z = block.getZ();
 
             String portalID = String.format("portal_%s_%d_%d_%d", worldName, Math.round(x), Math.round(y), Math.round(z));
+            String portalExclusion = String.format("portal_exclusion_%s_%d_%d_%d", worldName, Math.round(x), Math.round(y), Math.round(z));
 
             this.portalSet.createMarker(portalID, "Nether Portal", worldName, x, y, z, markerApi.getMarkerIcon("portal"), true);
+            this.portalExclusionSet.createCircleMarker(portalExclusion, "Nether Portal Zone", true, worldName, x, y, z, 100, 100, true);
             getLogger().info("Created Nether Portal: " + portalID);
+            getLogger().info("Created Nether Portal Exclusion: " + portalExclusion);
         }
     }
 
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event) {
         if (event.getBlock().getType().equals(Material.NETHER_PORTAL) == false) {
-
             return;
         }
 
         // nether portal being affected by physics means it's broken
-        getLogger().info("Portal destroyed");
+        //getLogger().info("Portal destroyed");
     }
 
     private void initialiseMarkerApi() {
@@ -85,11 +92,21 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
         this.portalSet = this.markerApi.getMarkerSet(SET_ID_PORTALS);
 
         if (this.portalSet == null) {
-            getLogger().info("Set not found, creating new set");
+            getLogger().info("Portals Set not found, creating new set");
             this.portalSet = this.markerApi.createMarkerSet(SET_ID_PORTALS, SET_NAME_PORTALS, null, true);
+        }
+        this.portalExclusionSet = this.markerApi.getMarkerSet(SET_ID_PORTAL_EXCLUSIONS);
+
+        if (this.portalExclusionSet == null) {
+            getLogger().info("Exclusions Set not found, creating new set");
+            this.portalExclusionSet = this.markerApi.createMarkerSet(SET_ID_PORTAL_EXCLUSIONS, SET_NAME_PORTAL_EXCLUSIONS, null, true);
         }
 
         this.portalSet.setHideByDefault(false);
-        getLogger().info("Set initialised");
+        getLogger().info("Portals Set initialised");
+
+        this.portalExclusionSet.setHideByDefault(false);
+        getLogger().info("Exclusions set initialised");
+
     }
 }
