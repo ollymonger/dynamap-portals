0b11a596928d33259ee11e9584e022857076a75d
Olly Monger <olly@yllo.co.uk>
2019-07-16 15:20:27 +0100

    Setting exclusion colours set to Purple (7931b0) and Portals/Exclusions hidden by default

--- src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
+++ src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
@@ -10,6 +10,7 @@ import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.DynmapCommonAPI;
+import org.dynmap.markers.CircleMarker;
 import org.dynmap.markers.Marker;
 import org.dynmap.markers.MarkerAPI;
 import org.dynmap.markers.MarkerSet;
@@ -61,7 +62,10 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
             String portalExclusion = String.format("portal_exclusion_%s_%d_%d_%d", worldName, Math.round(x), Math.round(y), Math.round(z));
 
             this.portalSet.createMarker(portalID, "Nether Portal", worldName, x, y, z, markerApi.getMarkerIcon("portal"), true);
-            this.portalExclusionSet.createCircleMarker(portalExclusion, "Nether Portal Zone", true, worldName, x, y, z, 512, 512, true);
+            CircleMarker exclusion = this.portalExclusionSet.createCircleMarker(portalExclusion, "Nether Portal Zone", true, worldName, x, y, z, 512, 512, true);
+            int fillcolor = Integer.parseInt("7931b0", 16);
+            exclusion.setFillStyle(0.3, fillcolor);
+
             getLogger().info("Created Nether Portal: " + portalID);
             getLogger().info("Created Nether Portal Exclusion: " + portalExclusion);
         }
@@ -90,15 +94,15 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
         this.portalExclusionSet = this.markerApi.getMarkerSet(SET_ID_PORTAL_EXCLUSIONS);
 
         if (this.portalExclusionSet == null) {
-            getLogger().info("Exclusions Set not found, creating new set");
-            this.portalExclusionSet = this.markerApi.createMarkerSet(SET_ID_PORTAL_EXCLUSIONS, SET_NAME_PORTAL_EXCLUSIONS, null, true);
+        getLogger().info("Exclusions Set not found, creating new set");
+        this.portalExclusionSet = this.markerApi.createMarkerSet(SET_ID_PORTAL_EXCLUSIONS, SET_NAME_PORTAL_EXCLUSIONS, null, true);
         }
 
-        this.portalSet.setHideByDefault(false);
+        this.portalSet.setHideByDefault(true);
         getLogger().info("Portals Set initialised");
 
-        this.portalExclusionSet.setHideByDefault(false);
+        this.portalExclusionSet.setHideByDefault(true);
         getLogger().info("Exclusions set initialised");
 
-    }
-}
+        }
+        }
