52e26e65fedad20a63fb6a9ea6209ddb1d357a5b
Olly Monger <olly@yllo.co.uk>
2019-07-25 08:58:57 +0100

    Code review

--- src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
+++ src/main/java/com/ollymonger/dynmap/portals/DynmapPortalsPlugin.java
@@ -19,7 +19,10 @@ import org.dynmap.markers.Marker;
 import org.dynmap.markers.MarkerAPI;
 import org.dynmap.markers.MarkerSet;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -118,15 +121,16 @@ public class DynmapPortalsPlugin extends JavaPlugin implements Listener {
         getLogger().info("Created Nether Portal: " + portalId);
         getLogger().info("Created Nether Portal Exclusion: " + portalExclusion);
 
-        this.writePortals();
+        writePortals();
+
     }
 
     private void writePortals() {
-        Type portalType = new TypeToken<RegisteredPortalSerializer>() {}.getType();
+        Type portalType = new TypeToken<List<RegisteredPortal>>() {}.getType();
         try{
             Path path = Paths.get(getDataFolder().getAbsolutePath(), "portals.json");
             File file = new File(path.toString());
-            file.mkdirs();
+            file.getParentFile().mkdirs();
             OutputStreamWriter os = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
 
             os.write(gson.toJson(registeredPortals, portalType));
